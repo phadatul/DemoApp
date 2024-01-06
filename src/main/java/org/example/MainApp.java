d9ac73e432749ec3bf4984afe896ee767152baa7
phadatul <phadatul@gmail.com>
2024-01-06 11:11:17 +0530

    corrected syntax error

--- src/main/java/org/example/MainApp.java
+++ src/main/java/org/example/MainApp.java
@@ -6,7 +6,7 @@ public class MainApp {
      * A main() so we can easily run these routing rules in our IDE
      */
     public static void main(String... args) throws Exception {
-        System.out.println("Hello from java")
+        System.out.println("Hello from java");
     }
 
 }
